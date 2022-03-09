ee22b8ceeee8108069d2af6418970869c9cb7563
FranciscoChavezS <francisco.csalado@alumnos.udg.mx>
2022-03-09 00:57:21 -0600

    Login Cel

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -274,14 +274,14 @@
                 </div>
             </div>
             @else
-            <div class="py-1 border-t border-gray-200">
-                <x-jet-responsive-nav-link href="{{ route('login') }}  }}" :active="request()->routeIs('login') ">
-                    Login
-                </x-jet-responsive-nav-link>
-                <x-jet-responsive-nav-link href="{{ route('register') }}" :active="request()->routeIs('register') ">
-                    Register
-                </x-jet-responsive-nav-link>
-            </div>
+            @else
+            <a href="{{ route('login') }}" class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:ring-purple-200 dark:focus:ring-purple-800">
+                <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                    Log in
+                </span>
+            </a>
+            
+            <a href="{{ route('register') }}" class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Register</a>
 
         @endauth
     </div>
