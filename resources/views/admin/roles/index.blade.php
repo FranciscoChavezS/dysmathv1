7d3a008e6e740c2a27af60055a01fc6080bf1983
FranciscoChavezS <francisco.csalado@alumnos.udg.mx>
2022-03-10 13:44:04 -0600

    Estilos 2.0

--- resources/views/admin/roles/index.blade.php
+++ resources/views/admin/roles/index.blade.php
@@ -17,32 +17,32 @@
         <div class="card-header">
             <a href="{{route('admin.roles.create')}}">Crear curso</a>
         </div>
-        <div class="card-body">
-            <table class="table table-striped table-responsive">
+        <div class="card-body p-0">
+            <table class="table table-striped">
                 <thead>
                     <tr>
-                        <th>ID</th>
-                        <th>Nombre</th>
-                        <th>Permisos </th>
-                        <th colspan="2"></th>
+                        <th scope="col">ID</th>
+                        <th scope="col">Nombre</th>
+                        <th scope="col">Permisos </th>
+                        <th colspan="2" scope="col"></th>
                     </tr>
                 </thead>
                 <tbody>
                     @forelse ($roles as $role)
                     <tr>
-                        <td>{{$role->id}}</td>
+                        <td scope="row">{{$role->id}}</td>
                         <td>{{$role->name}}</td>
-                        <td>
+                        <td scope="row">
                             @forelse ($role->permissions as $permission)
                                 <span class="badge badge-info">{{ $permission->name }}</span>
                             @empty
                                 <span class="badge badge-danger">Permisos no añadidos</span>
                             @endforelse
                         </td>
-                        <td width="10px">
+                        <td width="1rem">
                             <a class="btn btn-secondary"href="{{route('admin.roles.edit',$role)}}">Editar</a>
                         </td>
-                        <td width="10px">
+                        <td width="1rem">
                             <form action="{{route('admin.roles.destroy',$role)}}"method="post">
                             @method('delete')
                             @csrf
--- resources/views/layouts/instructor.blade.php
+++ resources/views/layouts/instructor.blade.php
@@ -35,25 +35,26 @@
                 <aside>
                     <h1 class="folt-bold text-lg mb-4">Edicion del curso</h1>
         {{--routeIs para metodo link activo--}}
-                    <ul class="mb-4 text-sm text-gray-600">
-                        <li class="leading-7 mb-1 border-l-4 @routeIs('instructor.courses.edit',$course) border-indigo-400 @else border-transparent  @endif pl-2">
-                            <a href="{{route('instructor.courses.edit',$course)}}">Informacion del curso</a>
-                        </li>
-                        <li class="leading-7 mb-1 border-l-4  @routeIs('instructor.courses.curriculum',$course) border-indigo-400 @else border-transparent  @endif pl-2">
-                            <a href="{{route('instructor.courses.curriculum',$course)}}">Leccion del curso</a>
-                        </li>
-                        <li class="leading-7 mb-1 border-l-4  @routeIs('instructor.courses.goals',$course) border-indigo-400 @else border-transparent  @endif pl-2">
-                            <a href="{{route('instructor.courses.goals',$course)}}">Metas del curso</a>
-                        </li>
-                        <li class="leading-7 mb-1 border-l-4 @routeIs('instructor.courses.students',$course) border-indigo-400 @else border-transparent  @endif pl-2">
-                            <a href="{{route('instructor.courses.students',$course)}}">Estudiantes del curso</a>
-                        </li>
-                        @if ($course->observation)
-                        <li class="leading-7 mb-1 border-l-4 @routeIs('instructor.courses.observation',$course) border-indigo-400 @else border-transparent  @endif pl-2">
-                            <a href="{{route('instructor.courses.observation',$course)}}">Observaciones del curso</a>
-                        </li>
-                        @endif
-                    </ul>
+        <ul class="mb-8">
+            <li>
+                <a class="block px-4 py-2 mb-1 border-l-4 @routeIs('instructor.courses.edit', $course) text-blue-700 bg-blue-100 hover:bg-blue-200 border-blue-400 @else text-gray-600 hover:bg-gray-200 border-transparent @endif" href="{{ route('instructor.courses.edit', $course) }}">Informacion del curso</a>
+            </li>
+            <li>
+                <a class="block px-4 py-2 mb-1 border-l-4 @routeIs('instructor.courses.curriculum', $course) text-blue-700 bg-blue-100 hover:bg-blue-200 border-blue-400 @else text-gray-600 hover:bg-gray-200 border-transparent @endif" href="{{ route('instructor.courses.curriculum', $course) }}">Crear lecciones</a>
+            </li>
+            <li>
+                <a class="block px-4 py-2 mb-1 border-l-4 @routeIs('instructor.courses.goals', $course) text-blue-700 bg-blue-100 hover:bg-blue-200 border-blue-400 @else text-gray-600 hover:bg-gray-200 border-transparent @endif" href="{{ route('instructor.courses.goals', $course) }}">Crear metas</a>
+            </li>
+            <li>
+                <a class="block px-4 py-2 mb-1 border-l-4 @routeIs('instructor.courses.students', $course) text-blue-700 bg-blue-100 hover:bg-blue-200 border-blue-400 @else text-gray-600 hover:bg-gray-200 border-transparent @endif" href="{{ route('instructor.courses.students', $course) }}">Estudiantes</a>
+            </li>
+
+            @if ($course->observation)
+                <li>
+                    <a class="block px-4 py-2 mb-1 border-l-4 @routeIs('instructor.courses.observation', $course) text-blue-700 bg-blue-100 hover:bg-blue-200 border-blue-400 @else text-gray-600 hover:bg-gray-200 border-transparent @endif" href="{{ route('instructor.courses.observation', $course) }}">Observaciones</a>
+                </li>
+            @endif
+        </ul>
 
                     @switch($course->status)
                         @case(1)
@@ -64,16 +65,16 @@
                         </form>
                             @break
                         @case(2)
-                        <div class="card text-gray-500">
-                            <div class="card-body flex-1 mb-4 col-span-4">
+                        <div class="card text-sm">
+                            <div class="card-body flex-1 col-span-4 bg-yellow-200">
                                 Este curso se encuentra en revision
                             </div>
                         </div>
                             
                             @break
                         @case(3)
-                        <div class="card text-gray-500">
-                            <div class="card-body flex-1 mb-4 col-span-4">
+                        <div class="card text-sm">
+                            <div class="card-body flex-1 col-span-4 bg-green-200">
                                 Este curso se encuentra publicado
                             </div>
                         </div>
@@ -85,7 +86,7 @@
                    
 
                 </aside>
-                <div class="col-span-4 card mt-4">
+                <div class="md:col-span-4 card mt-4">
                     <main class="card-body text-gray-600">
                         {{$slot}}
                     </main>
