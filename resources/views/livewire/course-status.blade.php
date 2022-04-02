243a8ed5efd85272a2fdd459bd3615a3b94d2f0b
FranciscoChavezS <francisco.csalado@alumnos.udg.mx>
2022-04-02 17:32:49 -0600

    Correciones

--- resources/views/livewire/course-status.blade.php
+++ resources/views/livewire/course-status.blade.php
@@ -11,60 +11,54 @@
                 {{$current->description->name}}
             </div>
             @endif   
-            <div class="flex justify-between mt-4">
-                {{--marcar como terminada--}}
-                <div class="flex items-center  cursor-pointer" wire:click="completed">
-                    @if ($current->completed)
-                    <i class="fa fa-toggle-on text-2xl text-blue-600 dark:text-blue-500"></i>
-                    @else
-<<<<<<< HEAD
-                        <i class="fa fa-toggle-off text-2xl text-gray-600 dark:text-gray-300"></i>
-=======
-                        <i class="fa fa-toggle-off text-2xl text-gray-600 dark:text-white"></i>
->>>>>>> 9299490e60a4b386198ab947063db3d3be708159
-                    @endif
-                    <p class="text-lg ml-2 dark:text-pink-300">Marcar como finalizada</p>
-                </div>
-                @if ($current->resource)
-                    <div class="flex items-center text-gray-600 cursor-pointer" wire:click="download">
-                        <i class="fa fa-download text-lg  "></i>
-                        <p class="text-sm ml-2">Descargar recursos</p>
-                    </div>
-
-                @endif
-            </div>
 
             {{-- Boton Siguiente y Anterior Personalizado --}}
-            <div class="flex justify-between items-center mt-5">
-                <div>
-                    @if ($this->previous)
-                        <button wire:click="changeLesson({{ $this->previous }})" class="block dark:text-white bg-white shadow h-12 px-4 rounded-lg text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 mr-2 lg:mr-4 mb-2 dark:bg-gray-700">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="inline text-xs text-gray-400 mr-1" viewBox="0 0 16 16">
-                                <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
-                            </svg>
-                            Anterior
+            <div class="flex justify-between mt-4">
+                    <div class="cursor-pointer" wire:click="completed">
+                        @if ($current->completed)
+                        <svg xmlns="http://www.w3.org/2000/svg" width="1.5rem" height="1.5rem" fill="currentColor" class="inline text-green-500" viewBox="0 0 16 16">
+                            <path d="M5 3a5 5 0 0 0 0 10h6a5 5 0 0 0 0-10H5zm6 9a4 4 0 1 1 0-8 4 4 0 0 1 0 8z"/>
+                        </svg>
+                        @else
+                        <svg xmlns="http://www.w3.org/2000/svg" width="1.5rem" height="1.5rem" fill="currentColor" class="inline text-gray-500" viewBox="0 0 16 16">
+                            <path d="M11 4a4 4 0 0 1 0 8H8a4.992 4.992 0 0 0 2-4 4.992 4.992 0 0 0-2-4h3zm-6 8a4 4 0 1 1 0-8 4 4 0 0 1 0 8zM0 8a5 5 0 0 0 5 5h6a5 5 0 0 0 0-10H5a5 5 0 0 0-5 5z"/>
+                        </svg>
+                        @endif
+                        Marcar unidad como terminada
+                    </div>
+                    @if ($current->resource)
+                        <button class="btn" wire:click="download">
+                            <svg class="inline h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                                <path fill-rule="evenodd" d="M6 2a2 2 0 00-2 2v12a2 2 0 002 2h8a2 2 0 002-2V7.414A2 2 0 0015.414 6L12 2.586A2 2 0 0010.586 2H6zm5 6a1 1 0 10-2 0v3.586l-1.293-1.293a1 1 0 10-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 11.586V8z" clip-rule="evenodd" />
+                            </svg> Descargar recurso
                         </button>
                     @endif
                 </div>
 
-                <div class="mt-2 mb-4">
-                    <div class="flex ml-2 items-center py-5">
-                        <figure class="flex-shrink-0 mr-4">
-                            <a href="{{$current->juego}}"><button type="button" class="lg:w-96 md:w-76 sm:w-48 border-b-4 border-gray-700 text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-md px-5 py-2.5 text-center mr-2 mb-2">Jugar</button></a>
-                        </figure>
-
-                <div>
-                    @if ($this->next)
-                        <button wire:click="changeLesson({{ $this->next }})" class="block dark:text-white bg-white shadow h-12 px-4 rounded-lg text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 mr-2 lg:mr-4 mb-2 dark:bg-gray-700">
-                            Siguiente
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class=" inline text-xs text-gray-400 ml-1" viewBox="0 0 16 16">
-                                <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
-                            </svg>
-                        </button>
-                    @endif
+                <div class="flex justify-between items-center mt-5">
+                    <div>
+                        @if ($this->previous)
+                            <button wire:click="changeLesson({{ $this->previous }})" class="block bg-white shadow h-12 px-4 rounded-lg text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 mr-2 lg:mr-4 mb-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="inline text-xs text-gray-400 mr-1" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
+                                </svg>
+                                Anterior
+                            </button>
+                        @endif
+                    </div>
+                        <a href="{{$current->juego}}"><button type="button" class="lg:w-96 md:w-76 sm:w-48 border-b-4 border-gray-700 text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-md px-5 py-2.5 text-center mr-2 mb-2">Jugar</button></a>
+                    <div>
+                        @if ($this->next)
+                            <button wire:click="changeLesson({{ $this->next }})" class="block bg-white shadow h-12 px-4 rounded-lg text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 mr-2 lg:mr-4 mb-2">
+                                Siguiente
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class=" inline text-xs text-gray-400 ml-1" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
+                                </svg>
+                            </button>
+                        @endif
+                    </div>
                 </div>
-            </div>
-         
+    
         </div>
 
         <div>
